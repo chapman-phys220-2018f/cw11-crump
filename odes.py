bf9f128577070d660e31d5988e2b2a4e8ae865c8
Natanael <alpay100@mail.chapman.edu>
2018-11-14 02:08:42 +0000

    edits

--- odes.py
+++ odes.py
@@ -5,6 +5,10 @@
 # Name: Conner Carnahan
 # Student ID: 1614309
 # Email: carna104@mail.chapman.edu
+#FULL NAME :NATANAEL ALPAY
+#ID        :002285534
+#email:alpay100@mail.chapman.edu
+
 # Course: PHYS220/MATH220/CPSC220 Fall 2018
 # Assignment: HW 10
 ###
@@ -12,6 +16,7 @@
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import array as ar
 
 
 def noteulermethod(f,g,t0,tf,f0,f1,dt = 0.001):
@@ -84,6 +89,38 @@ def heun(t0,tf,f0,f1,dt = 0.001):
         count += 1
         
     plotitokay(npt,uk,upk)
+    
+    
+    
+#def RungeKutta_2(0,tf,f0,f1,dt = 0.001):
+def RungeKutta_2( f, t0, t ):
+    n = len( t )
+    rk = np.ar([ t0 ]*n)
+    while count<n-1:
+        h = t[count+1] - t[count]
+        k1 = h * f( x[count], t[count] ) / 2.0
+        rk[count+1] = x[count] + h * f( x[count] + k1, t[count] + h / 2.0 )
+        count=count+1
+    """
+        f     - function 
+        x0    - the initial condition
+        t     - list or NumPy array of t values to compute solution at.
+
+    """
+def RungeKutta_4( f, t0, t ):
+    count=0
+    n = len(t)
+    rk4 = np.ar([t0]* n)
+    while count<n-1:
+        h = t[count+1] - t[count]
+        k1 = h * f( rk4[count], t[i] )
+        k2 = h * f( rk4[count] + 0.5 * k1, t[count] + 0.5 * h )
+        k3 = h * f( rk4[count] + 0.5 * k2, t[count] + 0.5 * h )
+        k4 = h * f( rk4[count] + k3, t[count+1] )
+        rk4[count+1] = rk4[count] + ( k1 + 2.0 * ( k2 + k3 ) + k4 ) / 6.0
+        count=count+1
+        
+
     
 def plotitokay(t,uk,upk):
     fig = plt.figure(figsize = (16,9))
